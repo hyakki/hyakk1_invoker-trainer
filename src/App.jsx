e408a480aca38594e7690a79d47e1e540eb59b1c
hyakk1 <me@hyakki.io>
2021-04-30 17:17:15 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Title from './components/Title'
+
+import './App.scss'
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app-inner">
+        <Title />
+      </div>
+    </div>
+  )
+}
+
+export default App
